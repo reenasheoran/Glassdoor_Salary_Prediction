65be40bfe7990b95b77605455ed0c392a1a8d9a5
Reena Sheoran <73627453+reenasheoran@users.noreply.github.com>
2021-06-13 20:32:00 -0600

    first commit

---
+++ src/data_collection.py
@@ -0,0 +1,9 @@
+import os
+import scraper as sp 
+import pandas as pd 
+
+path = "C:/Users/reena/Desktop/Salary_Prediction"
+
+df = sp.get_jobs('data scientist',1000, False, path, 15)
+
+df.to_csv('job_listings.csv', index = False)
